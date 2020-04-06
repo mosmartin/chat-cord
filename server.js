9aa0b5d7235f6bb1209b99968c2e9dda0b2c0708
Martin Macharia <mosmartin5@gmail.com>
2020-04-06 18:57:42 +0200

    Add a static folder
    Add the public directory to the server.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setup the port
 const port = process.env.PORT || 3000;
 
+// listen
 app.listen(port, () => console.log(`🚀 Server listening on port ${port}`));
