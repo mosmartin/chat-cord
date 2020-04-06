d4f75878425066ba8f6336613883c8a1d88ee8e8
Martin Macharia <mosmartin5@gmail.com>
2020-04-06 21:53:19 +0200

    Update server.js
    Add the socketio artifacts.

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+require('dotenv').config();
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
+// create the express app
 const app = express();
+
+// create the http server
+const server = http.createServer(app);
+
+// initialize socketio var
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// setup the port
-const port = process.env.PORT || 3000;
+// listen for the connection event
+io.on('connection', (socket) => {
+  // welcome message
+  socket.emit('message', 'Welcome to ChatCord!');
 
-// listen
-app.listen(port, () => console.log(`🚀 Server listening on port ${port}`));
+  // broadcast when a user connects
+  // broadcast to all users except the user who is connecting
+  socket.broadcast.emit('message', 'A user has joined the chat.');
+
+  // run when a client disconnets
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat.');
+  });
+
+  // listen for the chat message
+  socket.on('chatMessage', (msg) => {
+    // emit the message to everyone
+    io.emit('message', msg);
+  });
+});
+
+// setup the port
+const port = process.env.PORT || 5000;
+
+// listen for requests
+server.listen(port, () => console.log(`🚀 Server listening on port ${port}`));
